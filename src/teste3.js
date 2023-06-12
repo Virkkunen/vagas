54afc6dae2da476021822b02ea4a287ced3c0ef3
Virkkunen <vrkknn@vrkknn.net>
2023-06-12 09:39:03 -0300

    chore: delete by id instead of by name

---
+++ src/teste3.js
@@ -0,0 +1,17 @@
+const data = require("./fakeData");
+
+module.exports = function (req, res) {
+    const { id } = req.params;
+
+    // findIndex vai deixar ter uma confirmação que achou e deletou
+    const userIndex = data.findIndex((person) => person.id === id);
+
+    // se não achar o user, index é -1
+    if (userIndex !== -1) {
+        // remover user pelo índice
+        data.splice(userIndex, 1);
+        return res.status(204).end();
+    }
+
+    return res.status(404).json({ message: 'Usuário não encontrado.' });
+};
